9268614905de6693b765633d021cfe48c95bb56e
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-07-19 11:30:15 -0700

    Added constructors and additional functionality based on user input

--- program.cs
+++ program.cs
@@ -7,6 +7,14 @@ class Bread
   public int UnitPrice;
   public string DealDescription;
   public int DealPrice;
+
+  public Bread(string description, int unitPrice, string dealDescription, int dealPrice)
+  {
+    Description = description;
+    UnitPrice = unitPrice;
+    DealDescription = dealDescription;
+    DealPrice = dealPrice;
+  }
 }
 class Pastry
 {
@@ -15,22 +23,66 @@ class Pastry
   public string DealDescription;
   public int DealPrice;
 
+  public Pastry(string description, int unitPrice, string dealDescription, int dealPrice)
+  {
+    Description = description;
+    UnitPrice = unitPrice;
+    DealDescription = dealDescription;
+    DealPrice = dealPrice;
+  }
 }
 public class Program
 {
   public static void Main()
   {
-    Bread wheat = new Bread();
-    wheat.Description = "Whole Grain Goodness Wheat";
-    wheat.UnitPrice = 5;
-    wheat.DealDescription = "Buy 2, get 1 free.";
-    wheat.DealPrice = 10;
+    Bread wheat = new Bread("Hearty loaf of whole grain goodness", 5, "Buy 2, get 1 free.", 10);
 
-    Pastry croissant = new Pastry();
-    croissant.Description = "Layer after layer of flaky deliciousness";
-    croissant.UnitPrice = 2;
-    croissant.DealDescription = "Buy 3 for $5.";
-    croissant.DealPrice = 5;
+    Pastry croissant = new Pastry("Layer after layer of flaky deliciousness", 2, "Buy 3 for $5.", 5);
 
+    Console.WriteLine("Welcome to Pierre's Bakery");
+
+    List<Bread> Breads = new List<Bread>() { wheat };
+
+    foreach(Bread item in Breads)
+    {
+      Console.WriteLine("Price per loaf: $" + item.UnitPrice);
+    }
+
+    List<Pastry> Pastries = new List<Pastry>() { croissant };
+
+    foreach(Pastry item in Pastries)
+    {
+      Console.WriteLine("Price per pastry: $" + item.UnitPrice);
+    }
+
+    Console.WriteLine("If you would like to buy bread enter B. If you would like to buy pastries enter P.");
+    string buyingCheck = Console.ReadLine();
+    if(buyingCheck.ToUpper()== "B")
+    {
+      Console.WriteLine("Lets buy some bread!");
+      foreach(Bread item in Breads)
+      {
+        Console.WriteLine(item.Description);
+        Console.WriteLine("Flash Sale Alert: " + item.DealDescription);
+        Console.WriteLine("Enter the number of loaves you would like to buy: ");
+        string stringUnitsPurchasing = Console.ReadLine();
+        int unitsPurchasing = int.Parse(stringUnitsPurchasing);
+        Console.WriteLine(unitsPurchasing);
+      }
+    }
+    else if(buyingCheck.ToUpper()== "P")
+    {
+      Console.WriteLine("Lets buy some pastries!");
+      foreach(Pastry item in Pastries)
+      {
+        Console.WriteLine(item.Description);
+        Console.WriteLine("Flash Sale Alert: " + item.DealDescription);
+        Console.WriteLine("Enter the number of pastries you would like to buy: ");
+        string stringUnitsPurchasing = Console.ReadLine();
+        int unitsPurchasing = int.Parse(stringUnitsPurchasing);
+        Console.WriteLine(unitsPurchasing);
+      }
+    }
+    
   }
 }
