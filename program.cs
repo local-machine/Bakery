1e80bd132a5f29493ee8fb688741366d984b84a4
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-07-19 09:59:01 -0700

    Made classes for Pastry and Bread and created an instantiation of each within Main()

--- program.cs
+++ program.cs
@@ -1,12 +1,36 @@
 using System;
 using System.Collections.Generic;
 
-namespace Bakery {
-    public class Program 
-    {
-        public static void Main()
-        {
+class Bread
+{
+  public string Description;
+  public int UnitPrice;
+  public string DealDescription;
+  public int DealPrice;
+}
+class Pastry
+{
+  public string Description;
+  public int UnitPrice;
+  public string DealDescription;
+  public int DealPrice;
 
-        }
-    }
+}
+public class Program
+{
+  public static void Main()
+  {
+    Bread wheat = new Bread();
+    wheat.Description = "Whole Grain Goodness Wheat";
+    wheat.UnitPrice = 5;
+    wheat.DealDescription = "Buy 2, get 1 free.";
+    wheat.DealPrice = 10;
+
+    Pastry croissant = new Pastry();
+    croissant.Description = "Layer after layer of flaky deliciousness";
+    croissant.UnitPrice = 2;
+    croissant.DealDescription = "Buy 3 for $5.";
+    croissant.DealPrice = 5;
+
+  }
 }
